5cfe8919bc0f4ab0c9de8f97849c491692d366f2
Jim Stam <jimstam@hotmail.com>
2022-05-06 14:42:16 +0200

    Added ORDER BY clause to queries, created separate benchmark for checkpointing speed, add file to help downloading data

--- benchmark/publicbi/data/hashtags/queries/1.sql
+++ benchmark/publicbi/data/hashtags/queries/1.sql
@@ -1,1 +1,1 @@
-SELECT "HashTags_1"."twitter#user#screen_name" AS "twitter#user#screen_name" FROM "HashTags_1" GROUP BY "HashTags_1"."twitter#user#screen_name" LIMIT 44;
+SELECT "HashTags_1"."twitter#user#screen_name" AS "twitter#user#screen_name" FROM "HashTags_1" GROUP BY "HashTags_1"."twitter#user#screen_name" ORDER BY "HashTags_1"."twitter#user#screen_name" LIMIT 44;
