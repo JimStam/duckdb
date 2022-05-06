11e61e1172a0e87039b8a82560dc7a70a5347176
Jim Stam <jimstam@hotmail.com>
2022-05-06 16:30:01 +0200

    Adding more ORDER BY clauses

--- benchmark/publicbi/data/rentabilidad/queries/19.sql
+++ benchmark/publicbi/data/rentabilidad/queries/19.sql
@@ -1,1 +1,1 @@
-SELECT "Rentabilidad_1"."Ruta de Venta" AS "Ruta de Venta" FROM "Rentabilidad_1" WHERE (("Rentabilidad_1"."Locación" = 'Bogota Sur') AND ("Rentabilidad_1"."Zona" = 'CE')) GROUP BY "Rentabilidad_1"."Ruta de Venta";
+SELECT "Rentabilidad_1"."Ruta de Venta" AS "Ruta de Venta" FROM "Rentabilidad_1" WHERE (("Rentabilidad_1"."Locación" = 'Bogota Sur') AND ("Rentabilidad_1"."Zona" = 'CE')) GROUP BY "Rentabilidad_1"."Ruta de Venta" ORDER BY "Ruta de Venta";
